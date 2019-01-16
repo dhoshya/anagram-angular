df00e7539fcbca72557ae83dac0401c52af1866e
Rohan Joshi <joshi.rohan09@gmail.com>
2019-01-15 20:18:33 -0500

    added logic

---
+++ src/app/anagram.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AnagramService {
+
+  getAnagram(word): Promise<any> {
+    return Promise.resolve();
+  }
+
+  constructor() { }
+
+  //function genAnagram() takes in word typed in by the user.
+  //initializes a var anagram to keep track of the words spliced off in the for loop.
+  //initializes n to keep a track is user wants to display anagrams of length n.
+  //anagrams array where all the spliced words are pushed in.
+  //Returns: a set of anagram[] such that dups are avoided.
+
+  genAnagram(word, n = null, anagram ='', anagrams = []): any {
+
+    //to get rid of duplicates and lowercase and uppercase letters are treated
+    //as same
+    word = word.toUpperCase();
+
+    //this will push all the substreams to the array as well.
+    if(anagram){
+      anagrams.push(anagram);
+    }
+
+    if(!word){
+      return;
+    }
+
+    for(let i = 0; i < word.length; i++){
+      anagram = anagram + word[i];
+      this.genAnagram(word.slice(0,i) + word.slice(i+1), n, anagram, anagrams);
+      anagram = anagram.slice(0, anagram.length - 1);
+    }
+    let result = [...Array.from(new Set(anagrams))];
+
+    // the following code will give out anagrams of specified length "n"
+    let hash = {};
+    for ( let i = 0; i < result.length; i++){
+      anagram = result[i];
+      let len = anagram.length;
+      if (!(len in hash)){
+        hash[len] = [anagram];
+      } else {
+        hash[len].push(anagram);
+      }
+    }
+
+    return n == null ? result : hash[n];
+  };
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { AnagramService } from './anagram.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AnagramService]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  title = 'Anagram App for Fractal Industries';
+  anagrams = [];
+  isDesc: boolean;
+
+  constructor(private anagramService: AnagramService) {}
+
+  ngOnInit(): void {
+    this.isDesc = false;
+    }
+
+  getAnagrams(word): void {
+    this.anagramService.getAnagram(word)
+    .then(anagrams => this.anagrams = anagrams);
+  }
 }
